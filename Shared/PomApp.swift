0f70c93259906a5eca39ac737cdaa179180e10f9
selichter <lichter.sarahe@gmail.com>
2021-01-26 21:14:21 -0600

    initial commit

---
+++ Shared/PomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomApp.swift
+//  Shared
+//
+//  Created by Sarah Lichter on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
